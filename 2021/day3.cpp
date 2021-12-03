6984b686b60d3b7df3ec6baedc7f81731fe37a58
tamarisk <me@tamarisk.it>
2021-12-03 19:48:12 +0000

    day 3 p2 complete

--- 2021/day3.cpp
+++ 2021/day3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <utility>
 
 using namespace std;
 
@@ -42,6 +43,53 @@ int part1(bool test=false) {
     return stoi(gamma, nullptr, 2) * stoi(epsilon, nullptr, 2);
 }
 
+vector<string> recursive(vector<string> ip, string ltgt, int iter) {
+    vector<string> output;
+    char filter;
+    int zeros = 0;
+    int ones = 0;
+
+    if ( ip.size() == 1 ) {
+        return ip;
+    } else {
+        for (int i = 0; i < ip.size(); i++) {
+            if (ip[i][iter] == '0') {
+                zeros += 1;
+            } else {
+                ones += 1;
+            }
+        }
+
+        if (ltgt == "greater") {
+            filter = zeros > ones ? '0' : '1';
+        } else if (ltgt == "smaller") {
+            filter = zeros <= ones ? '0' : '1';
+        }
+
+        for ( int i = 0; i < ip.size(); i++ ) {
+            if (ip[i][iter] == filter) {
+                output.push_back(ip[i]);
+            }
+        }
+
+        return recursive(output, ltgt, iter+1);
+    }
+} 
+
+int part2(bool test=false) {
+    // test solution gamma = 10111
+    // test solution epsilon = 01010
+    // final solution = 230
+    vector<string> ip = parseInput(test);
+    sort(ip.begin(), ip.end());
+    
+    string gamma = recursive(ip, "greater", 0)[0];
+    string epsilon = recursive(ip, "smaller", 0)[0];
+
+    return stoi(gamma, nullptr, 2) * stoi(epsilon, nullptr, 2);
+}
+
 int main() {
     cout << "Part 1 solution: " << part1(false) << "\n"; 
+    cout << "Part 2 solution: " << part2(false) << "\n";
 }
